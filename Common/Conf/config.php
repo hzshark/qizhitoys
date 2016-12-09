7aa2ac0acff9dc1137fcddb8172c8f785186f7ea
hzshark <hzshark@gmail.com>
2016-12-09 18:01:03 +0800

    修改数据库连接

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
 	'DB_TYPE'           =>  'mysql',     	// 数据库类型
-    'DB_HOST'           =>  '120.26.125.14', 	// 服务器地址
+    'DB_HOST'           =>  '10.27.165.76', 	// 服务器地址
     'DB_NAME'           =>  'toys',        // 数据库名
-    'DB_USER'           =>  'root',     	// 用户名
-    'DB_PWD'            =>  'ssqian123',     	// 密码
+    'DB_USER'           =>  'toys',     	// 用户名
+    'DB_PWD'            =>  'qizhiwj888',     	// 密码
     'DB_PORT'           =>  '3306',     	// 端口
     'DB_PREFIX'         =>  '',      	// 数据库表前缀
     'DB_DEBUG'  		=>  true, 			// 数据库调试模式 开启后可以记录SQL日志
