71cb9e3c3c202ec4ac87e864e296e536d023cc7a
邵桦 <hua_shao@bestsign.cn>
2017-02-23 16:56:12 +0800

    add help header

--- Home/Controller/HelpController.class.php
+++ Home/Controller/HelpController.class.php
@@ -6,14 +6,13 @@ use Home\Service\Help;
 
 class HelpController extends Controller
 {
-   
+
     public function Index()
     {
         header("Content-Type:text/html; charset=utf-8");
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">';
         $help = new Help();
         $infos = $help->showHelp();
         $this->show($infos["info"], "utf-8");
     }
-    
-    
 }
